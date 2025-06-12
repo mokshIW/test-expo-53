0baa49a06ee2cfdea2eca19ae0ef84bdb9c99b6b
Moksh Keloo <moksh@integratedwebworks.com>
2025-06-12 17:09:47 -0400

    chore: update dependencies and configure NativeWind for styling
    - Added NativeWind and Tailwind CSS dependencies in package.json
    - Updated react-native-safe-area-context version to use caret notation
    - Configured TypeScript to include nativewind-env.d.ts
    - Created babel.config.js to integrate NativeWind with Babel
    - Added global.css for Tailwind CSS directives
    - Configured Metro bundler to use NativeWind with global.css
    - Created nativewind-env.d.ts for TypeScript support
    - Added tailwind.config.js for Tailwind CSS configuration

--- app/index.tsx
+++ app/index.tsx
@@ -2,14 +2,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 h-full items-center justify-center bg-white">
+      <Text className="text-2xl text-blue-900 underline">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
